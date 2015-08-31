348babfad2d8d7c38b7e2ab20cdf71031d52489d
vforgione <v.forgione@gmail.com>
2015-08-31 14:27:38 -0500

    updated dict unpacking

--- djbetty/serializers.py
+++ djbetty/serializers.py
@@ -4,16 +4,22 @@ from rest_framework import serializers
 class ImageFieldSerializer(serializers.Field):
 
     def to_representation(self, obj):
-        if obj is None or obj.id is None:
-            return None
-        data = {
-            "id": obj.id,
-        }
-        if obj.field.alt_field:
-            data["alt"] = obj.alt
-
-        if obj.field.caption_field:
-            data["caption"] = obj.caption
+        if isinstance(obj, dict):
+            data = {
+                "id": obj.get("id"),
+                "alt": obj.get("alt"),
+                "caption": obj.get("caption")
+            }
+        else:
+            if obj is None or obj.id is None:
+                return None
+            data = {
+                "id": obj.id,
+            }
+            if obj.field.alt_field:
+                data["alt"] = obj.alt
+            if obj.field.caption_field:
+                data["caption"] = obj.caption
         return data
 
     def to_internal_value(self, data):
